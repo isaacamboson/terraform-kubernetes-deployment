847c34645018f54bf5a951f8b42ae9f1e915e6de
Isaac Amboson <isaacamboson@gmail.com>
2024-07-25 12:06:11 -0400

    formatting tf files

--- alb.tf
+++ alb.tf
@@ -57,9 +57,9 @@ resource "aws_security_group" "lb-sg" {
   dynamic "ingress" {
     for_each = local.inbound_ports
     content {
-      from_port       = ingress.value
-      to_port         = ingress.value
-      protocol        = "tcp"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
